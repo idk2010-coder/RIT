712695810dc3a56f5ea939fbd0d759245e0ad102
idk2010-coder <emaileshan.rahman@gmail.com>
2024-08-28 16:45:17 +0400

    Add files via upload

---
+++ Math Trainer.py
@@ -0,0 +1,73 @@
+print("Welcome to Math Trainer")
+print("For the scratch version: https://scratch.mit.edu/projects/1048703257")
+print("Follow the commands below:")
+x = int(input("1 + 1"))
+if x == 2:
+    print("Correct")
+else:
+    print("Wrong")
+x = int(input("5-4"))
+if x == 1:
+    print("Correct")
+else:
+    print("wrong")
+x = int(input("10+43"))
+if x == 53:
+    print("Correct")
+else:
+    print("Wrong")
+x = int(input("98-90"))
+if x == 8:
+    print("correct")
+else:
+    print("wrong")
+x = int(input("Bob has 2 chocolates and his friend gives 5 more chocolates"))
+if x == 7:
+    print("correct")
+else:
+    print("Wrong")
+x = int(input("A boy has 7 marbles. He loses 5 marbles. How many does he have now"))
+if x == 2:
+    print("Correct")
+else:
+    print("Wrong")
+x = int(input("107+234"))
+if x == 341:
+    print("Correct")
+else:
+    print("Wrong")
+x = int(input("500-200"))
+if x == 300:
+    print("Correct")
+else:
+    print("Wrong")
+x = int(input("1*5"))
+if x == 5:
+    print("Correct")
+else:
+    print("Wrong")
+x = int(input("Robin had 500 rupees. He then earned 200 rupees. How many does he have now?"))
+if x == 700:
+    print("Correct")
+else:
+    print("Wrong")
+x = int(input("Rob has 200 rupees. He spent 100 of them on comics. How many does he have?"))
+if x == 100:
+    print("correct")
+else:
+    print("Wrong")
+x = int(input("8 / 4"))
+if x == 2:
+    print("Correct")
+else:
+    print("Wrong")
+x = int(input("20+10-15*20/5"))
+if x == 20+10-15*20/5:
+    print("Correct")
+else:
+    print("Wrong")
+x = int(input("x + 10 = 20"))
+if x == 10:
+    print("Correct")
+else:
+    print("Wrong")
